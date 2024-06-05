50e26f8833b9000f10d35440fe6634a99dedf3a8
Adi Kurniawan <kurniawanadi4556@gmail.com>
2024-06-05 15:42:13 +0700

    fix: validations.

--- resources/views/admin/landingPage/index.blade.php
+++ resources/views/admin/landingPage/index.blade.php
@@ -67,6 +67,9 @@
                             </label>
                             <input type="file" class="d-none" name="hero_section_image_1"
                                 onchange="previewImage(event,'preview1')" id="hero_section_image_1">
+                            @error('hero_section_image_1')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="col-md-4">
                             <label class="form-label" for="hero_section_image_2">
@@ -76,6 +79,9 @@
                             </label>
                             <input type="file" class="d-none" name="hero_section_image_2" id="hero_section_image_2"
                                 onchange="previewImage(event,'preview2')">
+                            @error('hero_section_image_2')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="col-md-4">
                             <label class="form-label" for="">
@@ -85,6 +91,9 @@
                             </label>
                             <input type="file" name="hero_section_image_3" id="hero_section_image_3" class="d-none"
                                 onchange="previewImage(event,'preview3')">
+                            @error('hero_section_image_3')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     <div class="d-flex flex-column mt-3">
@@ -92,36 +101,57 @@
                         <input type="number" name="manpower_channelled" min="0" id="manpower_channelled"
                             class="form-control" value="{{ $landingPage->manpower_channelled }}"
                             oldvalue="{{ $landingPage->manpower_channelled }}">
+                        @error('manpower_channelled')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column mt-3">
                         <label for="client" class="form-label">Client</label>
                         <input type="number" name="client" min="0" id="client" class="form-control"
                             value="{{ $landingPage->client }}" oldvalue="{{ $landingPage->client }}">
+                        @error('client')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column mt-3">
                         <label for="youtube" class="form-label">Youtube</label>
                         <input type="text" name="youtube" id="youtube" class="form-control"
                             value="{{ $landingPage->youtube }}" oldvalue="{{ $landingPage->youtube }}">
+                        @error('youtube')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column mt-3">
                         <label for="instagram" class="form-label">Instagram</label>
                         <input type="text" name="instagram" id="instagram" class="form-control"
                             value="{{ $landingPage->instagram }}" oldvalue="{{ $landingPage->instagram }}">
+                        @error('instagram')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column mt-3">
                         <label for="facebook" class="form-label">Facebook</label>
                         <input type="text" name="facebook" id="facebook" class="form-control"
                             value="{{ $landingPage->facebook }}" oldvalue="{{ $landingPage->facebook }}">
+                        @error('facebook')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column mt-3">
                         <label for="tiktok" class="form-label">Tiktok</label>
                         <input type="text" name="tiktok" id="tiktok" class="form-control"
                             value="{{ $landingPage->tiktok }}" oldvalue="{{ $landingPage->tiktok }}">
+                        @error('tiktok')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column mt-3">
                         <label for="twitter" class="form-label">Twitter</label>
                         <input type="text" name="twitter" id="twitter" class="form-control"
                             value="{{ $landingPage->twitter }}" oldvalue="{{ $landingPage->twitter }}">
+                        @error('twitter')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column mt-3">
                         <label for="operational_start_day" class="form-label">Hari Mulai Operasional</label>
@@ -141,6 +171,9 @@
                             <option value="sunday"
                                 {{ $landingPage->operational_start_day === 'sunday' ? 'selected' : '' }}>Minggu</option>
                         </select>
+                        @error('operational_start_day')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column mt-3">
                         <label for="operational_end_day" class="form-label">Hari Selesai Operasional</label>
@@ -160,18 +193,27 @@
                             <option value="sunday" {{ $landingPage->operational_end_day === 'sunday' ? 'selected' : '' }}>
                                 Minggu</option>
                         </select>
+                        @error('operational_end_day')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column mt-3">
                         <label for="operational_start_time" class="form-label">Waktu Mulai Operasional</label>
                         <input type="time" name="operational_start_time" id="operational_start_time"
                             class="form-control" value="{{ $landingPage->operational_start_time }}"
                             oldvalue="{{ $landingPage->operational_start_time }}">
+                        @error('operational_start_time')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column mt-3">
                         <label for="operational_end_time" class="form-label">Waktu Selesai Operasional</label>
                         <input type="time" name="operational_end_time" id="operational_end_time" class="form-control"
                             value="{{ $landingPage->operational_end_time }}"
                             oldvalue="{{ $landingPage->operational_end_time }}">
+                        @error('operational_end_time')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="d-flex justify-content-end">
                         <button type="submit" class="btn btn-primary mt-3">Submit</button>
