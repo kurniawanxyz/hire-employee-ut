e95ad22dd692980d897368d57cd664a234978842
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-06-14 10:54:07 +0700

    asdas

--- database/seeders/BranchSeeder.php
+++ database/seeders/BranchSeeder.php
@@ -1,60 +1,60 @@
 <?php
 
-namespace Database\Seeders;
+    namespace Database\Seeders;
 
-use App\Models\Branch;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+    use App\Models\Branch;
+    use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+    use Illuminate\Database\Seeder;
 
-class BranchSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    class BranchSeeder extends Seeder
     {
-        //
-        $data = [
-            "Jawa"=>[
-                "Semarang",
-                "Jakarta",
-                "Surabaya"
-            ],
-            "Sumatra" => [
-                "Medan",
-                "Pekanbaru",
-                "Tanjung Enim",
-                "Bandar Lampung",
-                "Palembang"
-            ],
-            "Kalimantan" => [
-                "Berau",
-                "Damai",
-                "Sangatta",
-                "Samarinda",
-                "Balikpapan",
-                "Batukajang",
-                "Tanjung",
-                "Banjarmasin"
-            ],
-            "Papua" =>[
-                "Sorong",
-                "Timika",
-                "Jayapura"
-            ],
-            "Sulawesi"=>[
-                "Ujungpadang",
-                "Manado"
-            ]
-            ];
-            foreach ($data as $i=> $zone) {
-                foreach ($zone as $city) {
-                    Branch::create([
-                       "city" => $city,
-                       "zone" => $i,
-                       "coordinate" => "111111111,11111111111",
-                    ]);
-               }
-            }
+        /**
+         * Run the database seeds.
+         */
+        public function run(): void
+        {
+            //
+            $data = [
+                "Jawa"=>[
+                    "Semarang",
+                    "Jakarta",
+                    "Surabaya"
+                ],
+                "Sumatra" => [
+                    "Medan",
+                    "Pekanbaru",
+                    "Tanjung Enim",
+                    "Bandar Lampung",
+                    "Palembang"
+                ],
+                "Kalimantan" => [
+                    "Berau",
+                    "Damai",
+                    "Sangatta",
+                    "Samarinda",
+                    "Balikpapan",
+                    "Batukajang",
+                    "Tanjung",
+                    "Banjarmasin"
+                ],
+                "Papua" =>[
+                    "Sorong",
+                    "Timika",
+                    "Jayapura"
+                ],
+                "Sulawesi"=>[
+                    "Ujungpadang",
+                    "Manado"
+                ]
+                ];
+                foreach ($data as $i=> $zone) {
+                    foreach ($zone as $city) {
+                        Branch::create([
+                        "city" => $city,
+                        "zone" => $i,
+                        "coordinate" => "111111111,11111111111",
+                        ]);
+                }
+                }
+        }
     }
-}
